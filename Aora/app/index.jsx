46c815bb9691a20a0312118d22a82fd2dddfbef9
petrinawjt <petrinawong1909@gmail.com>
2024-06-09 21:07:47 +0800

    nativewind, assets, constants, pages, tabs setup

--- Aora/app/index.jsx
+++ Aora/app/index.jsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Hello</Text>
       <StatusBar style="auto" />
+      <Link href="/home" style={{ color:'blue' }}>Go to Home</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
